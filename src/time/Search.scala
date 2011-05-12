5c21eb2a026e400cd3db11be019750b67482d74f
Gabor Angeli <angeli@cs.stanford.edu>
2011-05-11 19:22:40 -0700

    Search up to A*

--- src/time/Search.scala
+++ src/time/Search.scala
@@ -9,20 +9,17 @@ class SearchException(s:String,e:Throwable) extends RuntimeException(s,e) {
 	def this(e:Throwable) = this(null,e)
 }
 
-trait SearchState {
+trait SearchState{
 	def children:List[SearchState]
 	def isEndState:Boolean
 	def hasChildren:Boolean
 	def cost:Double = 0.0
 	def heuristic:Double = 0.0
+	def assertEnqueueable:Boolean = true
+	def assertDequeueable:Boolean = true
 }
 
-class Search(store:{
-				def enqueue(s:SearchState*):Unit
-				def dequeue():SearchState
-				def clear():Unit
-				def isEmpty:Boolean
-			}) {
+class Search[S <: SearchState : Manifest](store:Search.Store[S]){
 	val storeLock = new Lock
 	val SINK = new SearchState{
 		override def children = List[SearchState]()
@@ -32,7 +29,10 @@ class Search(store:{
 		override def heuristic:Double = 0
 	}
 	
-	def search(start:SearchState,result:(SearchState,Int)=>Boolean):Int = {
+	def search(
+			start:S,
+			result:(S,Int)=>Boolean,
+			timeout:Int):Int = {
 		//--Setup
 		//(initialize store)
 		storeLock.acquire
@@ -44,16 +44,23 @@ class Search(store:{
 		//--Search
 		while(shouldContinue && !store.isEmpty) {
 			//(get head)
-			val node:SearchState = store.dequeue
+			val node:S = store.dequeue
+			assert(node.assertDequeueable, ""+node+" is not dequeuable")
 			count += 1
 			//(return head if ok)
-			if(node.isEndState){
-				shouldContinue = result(node, count)
-			}
+			if(node.isEndState){ shouldContinue = result(node, count) }
+			//(timeout)
+			if(count >= timeout){ shouldContinue = false }
 			//(add children)
 			if(shouldContinue){
 				node.children.foreach( (s:SearchState) => {
-					store.enqueue(s)
+					try{
+						assert(s.assertEnqueueable, ""+s+" is not pushable")
+						store.enqueue(s.asInstanceOf[S]) //add
+					} catch {
+						case (ex:ClassCastException) => 
+							throw new SearchException("Child has bad type: " + s.getClass())
+					}
 				})
 			}
 		}
@@ -63,25 +70,28 @@ class Search(store:{
 		//(return)
 		count
 	}
-	def search(start:SearchState):Array[SearchState] = {
-		var results = List[SearchState]()
-		search(start, (result:SearchState,count:Int) => {
+	
+	def search(start:S,result:(S,Int)=>Boolean):Int
+		= search(start,result,java.lang.Integer.MAX_VALUE)
+	def search(start:S):Array[S] = {
+		var results = List[S]()
+		search(start, (result:S,count:Int) => {
 					results = result :: results
 					true
 				})
 		results.reverse.toArray
 	}
-	def search(start:SearchState,maxSize:Int):Array[SearchState] = {
-		var results = List[SearchState]()
-		search(start, (result:SearchState,count:Int) => {
+	def search(start:S,maxSize:Int):Array[S] = {
+		var results = List[S]()
+		search(start, (result:S,count:Int) => {
 					results = result :: results
 					results.length < maxSize
 				})
 		results.reverse.toArray
 	}
-	def best(start:SearchState):SearchState = {
-		var rtn:SearchState = null
-		search(start, (result:SearchState,count:Int) => {rtn = result; false})
+	def best(start:S):S = {
+		var rtn:S = null.asInstanceOf[S]
+		search(start, (result:S,count:Int) => {rtn = result; false})
 		if(rtn == null){
 			throw new SearchException("No solution")
 		}
@@ -92,27 +102,67 @@ class Search(store:{
 
 
 object Search {
-	def dfs = new Search(new scala.collection.mutable.Stack[SearchState]{
-			def enqueue(s:SearchState*):Unit = pushAll(s)
-			def dequeue():SearchState = pop()
-		})
-	def bfs = new Search(new scala.collection.mutable.Queue[SearchState])
-	def uniformCost = new Search(new PriorityQueue[SearchState]()(
-		new Ordering[SearchState]{
-			def compare(a:SearchState,b:SearchState) = {
-				if(a.cost < b.cost){ 1 }
-				else if(a.cost > b.cost){ -1 }
-				else { 0 }
+	type Store[S <: SearchState] = {
+		def enqueue(s:S*):Unit
+		def dequeue():S
+		def clear():Unit
+		def isEmpty:Boolean
+	}
+
+	def apply[S <: SearchState : Manifest](s:Store[S]) = new Search[S](s)
+
+	def cache[S <: SearchState](store:Store[S]):Store[S] = {
+		new {
+			val cache:Set[S] = new HashSet[S]
+			var next:S = null.asInstanceOf[S]
+			def enqueue(s:S*):Unit = { store.enqueue(s:_*) }
+			def dequeue():S = {
+				if(isEmpty) {
+					throw new NoSuchElementException
+				}
+				val rtn = next
+				next = null.asInstanceOf[S]
+				cache += rtn
+				rtn
 			}
-		}))
-	def astar = new Search(new PriorityQueue[SearchState]()(
-		new Ordering[SearchState]{
-			def compare(a:SearchState,b:SearchState) = {
-				if((a.cost+a.heuristic) < (b.cost+b.heuristic)){ 1 }
-				else if((a.cost+a.heuristic) > (b.cost+b.heuristic)){ -1 }
-				else { 0 }
+			def clear():Unit = { cache.clear; store.clear }
+			def isEmpty:Boolean = {
+				if(next != null){ return false } //have a next
+				if(store.isEmpty){ return true } //underlying collection empty
+				var cand:S = store.dequeue 
+				while(cache contains (cand)){  //find an element not in cache
+					if(store.isEmpty){ return true }
+					cand = store.dequeue
+				}
+				next = cand //set that element
+				false
 			}
-		}))
+		}
+	}
+
+	def DFS[S <: SearchState]:Store[S] = new scala.collection.mutable.Stack[S]{
+			def enqueue(s:S*):Unit = pushAll(s)
+			def dequeue():S = pop()
+		}
+	def BFS[S <: SearchState]:Store[S] = new scala.collection.mutable.Queue[S]
+	def UNIFORM_COST[S <: SearchState]:Store[S] = 
+		new PriorityQueue[S]()(
+			new Ordering[S]{
+				def compare(a:S,b:S) = {
+					if(a.cost < b.cost){ 1 }
+					else if(a.cost > b.cost){ -1 }
+					else { 0 }
+				}
+			})
+	def A_STAR[S <: SearchState]:Store[S] =
+		new PriorityQueue[S]()(
+			new Ordering[S]{
+				def compare(a:S,b:S) = {
+					if((a.cost+a.heuristic) < (b.cost+b.heuristic)){ 1 }
+					else if((a.cost+a.heuristic) > (b.cost+b.heuristic)){ -1 }
+					else { 0 }
+				}
+			})
 }
 
 
--- test/src/time/TestSearch.scala
+++ test/src/time/TestSearch.scala
@@ -3,6 +3,7 @@ package time
 import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
 import time._
+import Search._
 
 object FakeTree {
 	def node(name:String, childrenLst:List[SearchState],
@@ -16,51 +17,108 @@ object FakeTree {
 			override def toString:String = name
 		}
 	}
-	val node2 = node("node2", List[SearchState]()           , 5, 0)
-	val node4 = node("node4", List[SearchState]()           , 8, 1)
-	val node6 = node("node6", List[SearchState]()           , 6, 0)
-	val node5 = node("node5", List[SearchState](node6)      , 5, 0)
-	val node3 = node("node3", List[SearchState](node5)      , 4, 1)
-	val node1 = node("node1", List[SearchState](node4,node3), 1, 3)
-	val node0 = node("node0", List[SearchState](node2,node1), 5, 4)
+	val node2:SearchState = node("node2", List[SearchState]()           , 5, 0)
+	val node4:SearchState = node("node4", List[SearchState]()           , 8, 1)
+	val node6:SearchState = node("node6", List[SearchState]()           , 6, 0)
+	val node5:SearchState = node("node5", List[SearchState](node6)      , 5, 0)
+	val node3:SearchState = node("node3", List[SearchState](node5)      , 4, 1)
+	val node1:SearchState = node("node1", List[SearchState](node4,node3), 1, 3)
+	val node0:SearchState = node("node0", List[SearchState](node2,node1), 5, 4)
+}
+
+object Manhattan {
+	var EDGE = java.lang.Integer.MAX_VALUE
+}
+class Manhattan(val x:Int,val y:Int,val c:Double) extends SearchState {
+	def this(x:Int,y:Int) = this(x,y,0.0)
+	import scala.math.abs
+	//--Search Overrides
+	override def children:List[Manhattan] 
+		= List[Manhattan](
+			new Manhattan(x-1,y,c+1),new Manhattan(x+1,y,c+1),
+			new Manhattan(x,y-1,c+1),new Manhattan(x,y+1,c+1))
+	override def isEndState:Boolean = x == 0 && y == 0
+	override def hasChildren:Boolean = true
+	override def cost:Double = c
+	override def heuristic:Double= (abs(x)+abs(y)).asInstanceOf[Double]
+	//--Debug Overrides
+	override def assertDequeueable:Boolean = {
+		x < Manhattan.EDGE && y < Manhattan.EDGE
+	}
+	//--Standard Overrides
+	override def toString:String = "("+x+","+y+":"+c+")"
+	override def equals(o:Any):Boolean = o match {
+			case (m:Manhattan) => (m.x==x && m.y==y)
+			case _ => false
+		}
+	override def hashCode:Int = x.hashCode ^ (y.hashCode << 16)
+}
+
+
+class EuclideanManhattan(x:Int,y:Int,c:Double) extends Manhattan(x,y,c) {
+	def this(x:Int,y:Int) = this(x,y,0.0)
+	override def children:List[EuclideanManhattan] 
+		= List[EuclideanManhattan](
+			new EuclideanManhattan(x-1,y,c+1),new EuclideanManhattan(x+1,y,c+1),
+			new EuclideanManhattan(x,y-1,c+1),new EuclideanManhattan(x,y+1,c+1))
+	override def cost:Double = c
+	override def heuristic:Double= scala.math.sqrt((x*x+y*y).asInstanceOf[Double])
+	//--Debug Overrides
+	override def assertDequeueable:Boolean = {
+		x < Manhattan.EDGE && y < Manhattan.EDGE
+	}
 }
 
 
 class SearchSpec extends Spec with ShouldMatchers{
+	val BREAK = (s:SearchState,c:Int) => false
+	val IGNORE = (s:SearchState,c:Int) => true
 	describe("DFS") {
 		it("should be creatable"){ 
-			val dfs:Search = Search.dfs
+			val dfs:Search[SearchState] = Search(DFS)
 		}
 		it("works for Fake Tree"){
-			val dfs:Search = Search.dfs
+			val dfs:Search[SearchState] = Search(DFS)
 			val results:Array[SearchState] = dfs.search(FakeTree.node0)
 			results.length should be (3)
 			results(0) should be (FakeTree.node6)
 			results(1) should be (FakeTree.node4)
 			results(2) should be (FakeTree.node2)
-			
+		}
+		it("breaks for Manhattan"){
+			val dfs:Search[Manhattan] = Search(DFS)
+			val count = dfs.search(new Manhattan(5,5),BREAK,10000)
+			count should be (10000)
 		}
 	}
 	describe("BFS") {
 		it("should be creatable"){ 
-			val bfs:Search = Search.bfs
+			val bfs:Search[SearchState] = Search(BFS)
 		}
 		it("works for Fake Tree"){
-			val bfs:Search = Search.bfs
+			val bfs:Search[SearchState] = Search(BFS)
 			val results:Array[SearchState] = bfs.search(FakeTree.node0)
 			results.length should be (3)
 			results(0) should be (FakeTree.node2)
 			results(1) should be (FakeTree.node4)
 			results(2) should be (FakeTree.node6)
-			
+		}
+		it("works for Manhattan"){
+			val bfs:Search[Manhattan] = Search(BFS)
+			//(correctness)
+			val out:Manhattan = bfs.best(new Manhattan(5,5))
+			out should be (new Manhattan(0,0))
+			//(time)
+			val count = bfs.search(new Manhattan(5,5),BREAK)
+			count should be > (1000)
 		}
 	}
 	describe("Uniform Cost Search") {
 		it("should be creatable"){ 
-			val ucs:Search = Search.uniformCost
+			val ucs:Search[SearchState] = Search(UNIFORM_COST)
 		}
 		it("works for Fake Tree"){
-			val ucs:Search = Search.uniformCost
+			val ucs:Search[SearchState] = Search(UNIFORM_COST)
 			val results:Array[SearchState] = ucs.search(FakeTree.node0)
 			results.length should be (3)
 			results(0) should be (FakeTree.node2)
@@ -68,19 +126,94 @@ class SearchSpec extends Spec with ShouldMatchers{
 			results(2) should be (FakeTree.node4)
 			ucs.search(FakeTree.node0, (s,c)=>false) should be (4)
 		}
+		it("works for Manhattan"){
+			val ucs:Search[Manhattan] = Search(UNIFORM_COST)
+			//(correctness)
+			val out:Manhattan = ucs.best(new Manhattan(5,5))
+			out should be (new Manhattan(0,0))
+			//(time)
+			val count = ucs.search(new Manhattan(5,5),BREAK)
+			count should be > (1000)
+			//(consistency with BFS)
+			val bfs:Search[Manhattan] = Search(BFS)
+			val bfsCount:Int = bfs.search(new Manhattan(5,5),BREAK)
+			(scala.math.abs(count - bfsCount) < (0.05*count)) should be (true)
+		}
 	}
-	describe("AStar") {
+	describe("AStar (uncached)") {
 		it("should be creatable"){ 
-			val astar:Search = Search.astar
+			val astar:Search[SearchState] = Search(A_STAR)
 		}
 		it("works for Fake Tree"){
-			val astar:Search = Search.astar
+			val astar:Search[SearchState] = Search(A_STAR)
 			val results:Array[SearchState] = astar.search(FakeTree.node0)
 			results.length should be (3)
 			results(0) should be (FakeTree.node2)
 			results(1) should be (FakeTree.node6)
 			results(2) should be (FakeTree.node4)
 			astar.search(FakeTree.node0, (s,c)=>false) should be (3) //pruning
+		}
+		it("works for Manhattan"){
+			val astar:Search[Manhattan] = Search(A_STAR)
+			//(correctness)
+			val out:Manhattan = astar.best(new Manhattan(5,5))
+			out should be (new Manhattan(0,0))
+			//(time)
+			var count = astar.search(new Manhattan(5,5),BREAK)
+			count should be < (100)
+			//(mid time)
+			count = astar.search(new Manhattan(10,10),BREAK)
+			count should be < (2000)
+		}
+		it("breaks for Manhattan (large)"){
+			val astar:Search[Manhattan] = Search(A_STAR)
+			//(long time)
+			val count = astar.search(new Manhattan(100,100),BREAK,100000)
+			count should be (100000)
+		}
+	}
+	describe("AStar (cached)"){
+		it("should be creatable"){ 
+			val astar:Search[SearchState] = Search(cache(A_STAR))
+		}
+		it("works for Fake Tree"){
+			val astar:Search[SearchState] = Search(cache(A_STAR))
+			val results:Array[SearchState] = astar.search(FakeTree.node0)
+			results.length should be (3)
+			results(0) should be (FakeTree.node2)
+			results(1) should be (FakeTree.node6)
+			results(2) should be (FakeTree.node4)
+			astar.search(FakeTree.node0, (s,c)=>false) should be (3) //pruning
+		}
+		it("works for Manhattan"){
+			val astar:Search[Manhattan] = Search(cache(A_STAR))
+			//(correctness)
+			val out:Manhattan = astar.best(new Manhattan(5,5))
+			out should be (new Manhattan(0,0))
+			//(time)
+			var count = astar.search(new Manhattan(5,5),BREAK)
+			count should be < (100)
+			//(mid time)
+			count = astar.search(new Manhattan(10,10),BREAK)
+			count should be < (200)
+		}
+		it("works for Manhattan (large)"){
+			val astar:Search[Manhattan] = Search(cache(A_STAR))
+			//(long time)
+			Manhattan.EDGE=102
+			val count = astar.search(new Manhattan(100,100),BREAK)
+			count should be < (10000)
+			Manhattan.EDGE=java.lang.Integer.MAX_VALUE
+		}
+		it("works for Imprecise Manhattan"){
+			val astar:Search[EuclideanManhattan] = Search(cache(A_STAR))
+			//(long time)
+			val count = astar.search(new EuclideanManhattan(100,100),BREAK)
+			count should be < (100000)
+			//(better than ucs)
+			val ucs:Search[EuclideanManhattan] = Search(cache(UNIFORM_COST))
+			val ucsCount = ucs.search(new EuclideanManhattan(100,100),BREAK)
+			count should be < (ucsCount)
 		}
 	}
 }
